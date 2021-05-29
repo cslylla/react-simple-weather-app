56d32859199230ed42fb5e974f4b2616d06da7c6
cslylla <81328857+cslylla@users.noreply.github.com>
2021-05-29 17:00:33 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <p>
-          This project was coded by <a href="https://www.linkedin.com/in/lilla-csanaky-a850a617/" target="_blank" title="Open Lilla`s LinkedIn profile" >cslylla</a> and it is <a href="https://github.com/cslylla" target="_blank" title="Open Lilla`s GitHub profile">open-sourced</a>. 
-        </p>
-      </footer>
+    <div className="container">
+      <h1>Weather App</h1>
+    <button type="button" class="btn btn-danger">Danger</button>
+<footer>
+    <p>
+      This is an <a href="https://github.com/cslylla" target="_blank" title="Open Lilla`s GitHub profile">open-sourced</a> project coded by <a href="https://www.linkedin.com/in/lilla-csanaky-a850a617/" target="_blank" title="Open Lilla`s LinkedIn profile" >cslylla</a> 
+    </p>
+
+</footer>
+    </div>
     </div>
   );
 }
