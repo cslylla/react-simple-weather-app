edac4aa32d43d2f0f94980f49e0a075815f19a4b
cslylla <81328857+cslylla@users.noreply.github.com>
2021-05-29 16:29:44 +0200

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        <p>
+          This project was coded by <a href="https://www.linkedin.com/in/lilla-csanaky-a850a617/" target="_blank" title="Open Lilla`s LinkedIn profile" >cslylla</a> and it is <a href="https://github.com/cslylla" target="_blank" title="Open Lilla`s GitHub profile">open-sourced</a>. 
+        </p>
+      </footer>
     </div>
   );
 }
